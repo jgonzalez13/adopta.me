53e105914314ac7cae5f40f80d2d8268717b0e84
Emmanuel Gonzalez <jgonzalez13@ucol.mx>
2019-11-02 05:36:32 -0600

    formulario

--- front/src/components/views/Home.container.jsx
+++ front/src/components/views/Home.container.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import useForm from 'react-hook-form';
 
-const Home = () => <div>sin autentificacion</div>;
+const Home = () => {
+  const { handleSubmit, register, errors } = useForm();
 
+  const onSubmit = values => {
+    console.log(values);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        name="email"
+        ref={register({
+          required: 'Required'
+        })}
+      />
+      {errors.email && errors.email.message}
+
+      <input
+        name="username"
+        type="password"
+        ref={register({
+          required: 'Required'
+        })}
+      />
+      {errors.username && errors.username.message}
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 export default Home;
