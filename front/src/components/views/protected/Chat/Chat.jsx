0c15f5454d6b18298ef5c917e1f2ba3dc8c1a8b0
JonathanSC <jonathansolisc_22@hotmail.com>
2019-11-03 08:16:43 -0600

    Modified chat

--- front/src/components/views/protected/Chat/Chat.jsx
+++ front/src/components/views/protected/Chat/Chat.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import SendBird from 'sendbird';
 
 //Import Styles
 import './Chat.sass';
 
 const Chat = () => {
+  var sb = new SendBird({ appId: '011AEB3D-EB9D-4296-B166-6B27FEAA4596' });
+  let userID = window.prompt('Ingresa un ID');
+  sb.connect(userID, function(user, error) {
+    if (error) {
+      return;
+    }
+  });
+  sb.OpenChannel.createChannel(function(openChannel, error) {
+    if (error) {
+      return;
+    }
+  });
+
+  sb.OpenChannel.getChannel('http://localhost:3000/chat/15', function(
+    openChannel,
+    error
+  ) {
+    if (error) {
+      return;
+    }
+
+    openChannel.enter(function(response, error) {
+      if (error) {
+        return;
+      }
+    });
+  });
+
+  openChannel.sendUserMessage('hola', 'data pos que', CUSTOM_TYPE, function(
+    message,
+    error
+  ) {
+    if (error) {
+      return;
+    }
+  });
+
   return <div className="main-chat-container">hola</div>;
 };
 
